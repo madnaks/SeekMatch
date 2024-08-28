912604c11335024afe0c049a749a0a68b5fe2ecf
madnaks <med.haouam@gmail.com>
2024-08-28 15:36:15 -0400

    Create Shared module and move header component from core to share

--- SeekMatch-Client/src/app/shared/shared.module.ts
+++ SeekMatch-Client/src/app/shared/shared.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HeaderComponent } from './components/header/header.component';
 import { TranslateModule } from '@ngx-translate/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
-  declarations: [],
+  declarations: [HeaderComponent],
   imports: [
     CommonModule,
     NgbModule,
     TranslateModule
-  ]
+  ],
+  exports: [HeaderComponent]
 })
-
-export class CoreModule { }
+export class SharedModule { }
