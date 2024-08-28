8d146baaa5de2ed7407e16f9ccb3a36107a2b4d6
madnaks <med.haouam@gmail.com>
2024-08-28 15:37:03 -0400

    Add color pallets

--- SeekMatch-Client/src/app/app.component.ts
+++ SeekMatch-Client/src/app/app.component.ts
@@ -3,11 +3,20 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { CoreModule } from "./core/core.module";
 import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { SharedModule } from './shared/shared.module';
+import { HomeModule } from './home/home.module';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, CoreModule, TranslateModule],
+  imports: [
+    CommonModule, 
+    RouterOutlet,
+    HomeModule,
+    CoreModule, 
+    SharedModule, 
+    TranslateModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
