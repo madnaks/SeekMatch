80cc5afba7a59185bea9e88c9c64c42c5179cc3f
madnaks <med.haouam@gmail.com>
2024-09-08 13:32:59 -0400

    Add auth routing module to the navbar

--- SeekMatch-Client/src/app/auth/auth.module.ts
+++ SeekMatch-Client/src/app/auth/auth.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SignUpComponent } from './components/sign-up/sign-up.component';
 import { LogInComponent } from './components/log-in/log-in.component';
+import { AuthRoutingModule } from './auth.routing.module';
 
 @NgModule({
   declarations: [
@@ -9,7 +10,8 @@ import { LogInComponent } from './components/log-in/log-in.component';
     LogInComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    AuthRoutingModule
   ]
 })
 export class AuthModule { }
