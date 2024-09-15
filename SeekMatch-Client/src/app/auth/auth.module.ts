832990a30320935ab614bddd01577f1dc82d3fcc
madnaks <med.haouam@gmail.com>
2024-09-15 09:39:30 -0400

    Delete login component

--- SeekMatch-Client/src/app/auth/auth.module.ts
+++ SeekMatch-Client/src/app/auth/auth.module.ts
@@ -1,15 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SignUpComponent } from './components/sign-up/sign-up.component';
-import { LogInComponent } from './components/log-in/log-in.component';
 import { AuthRoutingModule } from './auth.routing.module';
 import { TranslateModule } from '@ngx-translate/core';
 import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [
-    SignUpComponent,
-    LogInComponent
+    SignUpComponent
   ],
   imports: [
     CommonModule,
--- SeekMatch-Client/src/app/auth/auth.routing.module.ts
+++ SeekMatch-Client/src/app/auth/auth.routing.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SignUpComponent } from './components/sign-up/sign-up.component';
-import { LogInComponent } from './components/log-in/log-in.component';
 
 const routes: Routes = [
-  { path: 'sign-up', component: SignUpComponent },
-  { path: 'log-in', component: LogInComponent }
+  { path: 'sign-up', component: SignUpComponent }
 ];
 
 @NgModule({
