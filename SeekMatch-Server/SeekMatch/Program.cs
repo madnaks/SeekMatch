a7a7c91e76b1ac474688cb699e3a3bf4dd2791ba
madnaks <med.haouam@gmail.com>
2024-09-18 16:20:38 -0400

    Backend - Update SeekMatch

--- SeekMatch-Server/SeekMatch/Program.cs
+++ SeekMatch-Server/SeekMatch/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using SeekMatch_Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -8,6 +11,9 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+builder.Services.AddEntityFrameworkNpgsql().AddDbContext<SeekMatchDbContext>(opt =>
+    opt.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
