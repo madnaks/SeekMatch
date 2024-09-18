d6edcfdff2f67c4b8c2b54c6416e7d24b8ef48a9
madnaks <med.haouam@gmail.com>
2024-09-18 16:19:41 -0400

    Backend - Add SeekMatch-Core Project

---
+++ SeekMatch-Server/SeekMatch-Core/Entities/JobSeeker.cs
@@ -0,0 +1,10 @@
+﻿namespace SeekMatch_Core.Entities
+{
+    public class JobSeeker: BaseEntity
+    {
+        public required string FirstName { get; set; }
+        public required string LastName { get; set; }
+        public required User User { get; set; }
+
+    }
+}
---
+++ SeekMatch-Server/SeekMatch-Core/Entities/User.cs
@@ -0,0 +1,18 @@
+﻿namespace SeekMatch_Core.Entities
+{
+    public class User: BaseEntity
+    {
+        public required string Email { get; set; }
+        public required string Password { get; set; }
+        public UserRole Role { get; set; }
+        public JobSeeker? JobSeeker { get; set; }
+        public Recruiter? Recruiter { get; set; }
+
+    }
+    public enum UserRole
+    {
+        JobSeeker = 1,
+        Recruiter = 2
+    }
+
+}
