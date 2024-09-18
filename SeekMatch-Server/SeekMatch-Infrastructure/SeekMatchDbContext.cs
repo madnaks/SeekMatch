b0b55d99712799dcfb58fbfe801657d81752ca05
madnaks <med.haouam@gmail.com>
2024-09-18 16:20:13 -0400

    Backend - Add SeekMatch-Infrastructure project

---
+++ SeekMatch-Server/SeekMatch-Infrastructure/SeekMatchDbContext.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SeekMatch_Core.Entities;
+
+namespace SeekMatch_Infrastructure
+{
+    public class SeekMatchDbContext : DbContext
+    {
+        public SeekMatchDbContext(DbContextOptions<SeekMatchDbContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<JobSeeker> JobSeekers { get; set; }
+        public DbSet<Recruiter> Recruiters { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Configure relationships
+            modelBuilder.Entity<User>()
+                .HasOne(u => u.JobSeeker)
+                .WithOne(js => js.User)
+                .HasForeignKey<JobSeeker>(js => js.Id);
+
+            modelBuilder.Entity<User>()
+                .HasOne(u => u.Recruiter)
+                .WithOne(r => r.User)
+                .HasForeignKey<Recruiter>(r => r.Id);
+
+            // Default value configuration for CreatedAt and UpdatedAt in BaseEntity
+            modelBuilder.Entity<BaseEntity>()
+                .Property(b => b.CreatedAt)
+                .HasDefaultValueSql("getutcdate()");
+
+            modelBuilder.Entity<BaseEntity>()
+                .Property(b => b.UpdatedAt)
+                .HasDefaultValueSql("getutcdate()");
+        }
+
+    }
+}
