6f71535a1c32e76cd1d8cbffb192a13136712e80
madnaks <med.haouam@gmail.com>
2024-09-29 13:51:49 -0400

    Add dtos to application layer

---
+++ SeekMatch-Server/SeekMatch.Application/DTOs/RegisterDto.cs
@@ -0,0 +1,25 @@
+﻿using SeekMatch.Core.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace SeekMatch.Application.DTOs
+{
+    public class RegisterDto
+    {
+        [Required]
+        [EmailAddress]
+        public required string Email { get; set; }
+
+        [Required]
+        [MinLength(6)]
+        public required string Password { get; set; }
+
+        [Required]
+        public required string FirstName { get; set; }
+
+        [Required]
+        public required string LastName { get; set; }
+
+        [Required]
+        public UserRole Role { get; set; }
+    }
+}
