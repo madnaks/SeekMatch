abd8c61eb646b83fc15c49eb473227aedf736252
madnaks <med.haouam@gmail.com>
2024-10-11 13:12:50 -0400

    Backend - Add TalentController

---
+++ SeekMatch-Server/SeekMatch/Controllers/TalentController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using SeekMatch.Core.Entities;
+using System.Security.Claims;
+
+namespace SeekMatch.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TalentController : ControllerBase
+    {
+        private readonly UserManager<User> _userManager;
+
+        public TalentController(UserManager<User> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [Authorize]
+        [HttpGet("profile")]
+        public async Task<IActionResult> GetProfile()
+        {
+            var userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
+            if (userId == null)
+            {
+                return Unauthorized();
+            }
+
+            var talent = await _userManager.FindByIdAsync(userId);
+
+            if (talent == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(talent);
+        }
+    }
+}
