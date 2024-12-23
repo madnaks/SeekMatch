13dfc3396b47b5e28314d4150609963955abeeb5
madnaks <med.haouam@gmail.com>
2024-12-23 16:20:04 -0500

    Register recruiter - Backend - Add new column IsFreelancer to DB

---
+++ SeekMatch-Server/SeekMatch.Infrastructure/Migrations/20241223204342_AddIsFreelancerColumn.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SeekMatch.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIsFreelancerColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsFreelancer",
+                table: "Recruiters",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsFreelancer",
+                table: "Recruiters");
+        }
+    }
+}
