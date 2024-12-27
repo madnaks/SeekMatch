a7312a2c2ad642d710cf2be5095e0a46aeb1e615
madnaks <med.haouam@gmail.com>
2024-12-27 11:16:56 -0500

    Backend - Separate Register for Recruiter role

---
+++ SeekMatch-Server/SeekMatch.Application/DTOs/Recruiter/RegisterRecruiterDto.cs
@@ -0,0 +1,22 @@
+﻿using SeekMatch.Core.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace SeekMatch.Application.DTOs.Recruiter
+{
+    public class RegisterRecruiterDto
+    {
+        [Required]
+        [EmailAddress]
+        public required string Email { get; set; }
+
+        [Required]
+        [MinLength(6)]
+        public required string Password { get; set; }
+
+        [Required]
+        public required string FirstName { get; set; }
+
+        [Required]
+        public required string LastName { get; set; }
+    }
+}
---
+++ SeekMatch-Server/SeekMatch/Controllers/Recruiter/RecruiterController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using SeekMatch.Application.DTOs.Recruiter;
+using SeekMatch.Application.Interfaces;
+using SeekMatch.Core.Entities;
+
+namespace SeekMatch.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RecruiterController : ControllerBase
+    {
+        private readonly UserManager<User> _userManager;
+        private readonly IRecruiterService _recruiterService;
+
+        public RecruiterController(UserManager<User> userManager, IRecruiterService recruiterService)
+        {
+            _userManager = userManager;
+            _recruiterService = recruiterService;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] RegisterRecruiterDto registerRecruiterDto)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var user = new User
+            {
+                UserName = registerRecruiterDto.Email,
+                Email = registerRecruiterDto.Email,
+            };
+
+            var result = await _userManager.CreateAsync(user, registerRecruiterDto.Password);
+
+            if (!result.Succeeded)
+                return BadRequest(result.Errors);
+
+            var recruiter = new Recruiter()
+            {
+                FirstName = registerRecruiterDto.FirstName,
+                LastName = registerRecruiterDto.LastName,
+                IsFreelancer = true,
+                User = user
+            };
+
+            await _recruiterService.CreateAsync(recruiter);
+
+            return Ok(result);
+        }
+    }
+}
