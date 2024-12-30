8001b88a0530ec1363455a8f83b39a8cfd817a08
madnaks <med.haouam@gmail.com>
2024-12-30 13:25:29 -0500

    Backend - Add ProfilePicture column to Recruiter

--- SeekMatch-Server/SeekMatch.Core/Entities/Recruiter.cs
+++ SeekMatch-Server/SeekMatch.Core/Entities/Recruiter.cs
@@ -5,6 +5,7 @@
         public required string FirstName { get; set; }
         public required string LastName { get; set; }
         public bool IsFreelancer { get; set; }
+        public byte[]? ProfilePicture { get; set; }
         public required User User { get; set; }
         public Company? Company { get; set; }
         public string? CompanyId { get; set; }
---
+++ SeekMatch-Server/SeekMatch.Infrastructure/Migrations/20241230182414_AddProfilePictureToRecruiter.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SeekMatch.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddProfilePictureToRecruiter : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte[]>(
+                name: "ProfilePicture",
+                table: "Recruiters",
+                type: "bytea",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ProfilePicture",
+                table: "Recruiters");
+        }
+    }
+}
