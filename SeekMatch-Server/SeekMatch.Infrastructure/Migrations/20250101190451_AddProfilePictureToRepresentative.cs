a2fe4035a52d73dda46c6a9754be3e865b693cf3
madnaks <med.haouam@gmail.com>
2025-01-01 14:06:00 -0500

    Backend - Add ProfilePicture Column To Representative

---
+++ SeekMatch-Server/SeekMatch.Infrastructure/Migrations/20250101190451_AddProfilePictureToRepresentative.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SeekMatch.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddProfilePictureToRepresentative : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte[]>(
+                name: "ProfilePicture",
+                table: "Representatives",
+                type: "bytea",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ProfilePicture",
+                table: "Representatives");
+        }
+    }
+}
