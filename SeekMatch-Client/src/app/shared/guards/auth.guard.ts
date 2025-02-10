4fe2b13d95554ac3ddf1789b237d1b8b72d34466
madnaks <med.haouam@gmail.com>
2025-02-09 20:01:01 -0500

    Progress on about you component

--- SeekMatch-Client/src/app/shared/guards/auth.guard.ts
+++ SeekMatch-Client/src/app/shared/guards/auth.guard.ts
@@ -1,6 +1,6 @@
 import { Router } from '@angular/router';
 import { inject } from '@angular/core';
-import { AuthService } from '../shared/services/auth.service';
+import { AuthService } from '../services/auth.service';
 
 export const authGuard = () => {
   const authService = inject(AuthService);
