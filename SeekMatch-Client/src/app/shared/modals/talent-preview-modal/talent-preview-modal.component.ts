ff458a422ee3b62f958ab184f86ded82a0060406
madnaks <med.haouam@gmail.com>
2025-02-14 14:27:06 -0500

    Frontend - Talent Preview Modal inhance basic information

--- SeekMatch-Client/src/app/shared/modals/talent-preview-modal/talent-preview-modal.component.ts
+++ SeekMatch-Client/src/app/shared/modals/talent-preview-modal/talent-preview-modal.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { TalentService } from '../../services/talent.service';
 import { Talent } from '../../models/talent';
+import { SafeUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-talent-preview-modal',
@@ -15,6 +16,8 @@ export class TalentPreviewModalComponent implements OnInit {
   @Input() selectedTalentId: string = "";
   
   public currentTalent: Talent | null = null;
+  public profilePicture: SafeUrl | string | null = null;
+  public isSummaryExpanded = false;
 
   constructor(private talentService: TalentService) {
   }
@@ -27,12 +30,22 @@ export class TalentPreviewModalComponent implements OnInit {
     } else if (this.selectedTalent !== null) {
       this.currentTalent = this.selectedTalent;
     }
+
+    if (this.currentTalent?.profilePicture) {
+      this.profilePicture = `data:image/jpeg;base64,${this.currentTalent.profilePicture}`;
+    } else {
+      this.profilePicture = "../../../assets/images/male-default-profile-picture.svg";
+    }
   }
 
   public dismiss(reason: string): void {
     if (this.dismissModal) {
       this.dismissModal(reason);
     }
+  }
+
+  public get hasLongSummary(): boolean {
+    return !!this.currentTalent?.summary && this.currentTalent.summary.length > 150;
   }
 
 }
