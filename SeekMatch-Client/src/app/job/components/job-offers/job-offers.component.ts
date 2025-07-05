39521265d3b5650663969dfb4f9ebcbb8dace070
madnaks <med.haouam@gmail.com>
2025-07-05 18:21:22 -0400

    Frontend - Improve job offers responsiveness

--- SeekMatch-Client/src/app/job/components/job-offers/job-offers.component.ts
+++ SeekMatch-Client/src/app/job/components/job-offers/job-offers.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { JobOffer } from '../../../shared/models/job-offer';
+import { AuthService } from '../../../shared/services/auth.service';
 
 @Component({
   selector: 'app-job-offers',
@@ -11,14 +12,23 @@ export class JobOffersComponent implements OnInit {
   public selectedJobOffer: JobOffer | null = null;
   public isMobileView: boolean = false;
   public showDetailsOnMobile: boolean = false;
+  public canApply: boolean = false;
+  public isSaving: boolean = false;
+  
+  constructor(private authService: AuthService) {
+  }
 
   ngOnInit(): void {
     this.checkMobileView();
     window.addEventListener('resize', this.checkMobileView.bind(this));
+    this.canApply = this.authService.canApply();
   }
 
   private checkMobileView(): void {
     this.isMobileView = window.innerWidth < 768;
+    if (this.isMobileView) {
+      this.selectedJobOffer = null;
+    }
   }
 
   public onJobOfferSelected(jobOffer: any): void {
