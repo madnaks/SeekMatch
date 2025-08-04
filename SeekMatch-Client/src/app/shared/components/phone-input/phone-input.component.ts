1cd81c32932d64e30c59cf88c70e308a27945168
madnaks <med.haouam@gmail.com>
2025-08-03 21:41:30 -0400

    Frontend - Create phone input component

---
+++ SeekMatch-Client/src/app/shared/components/phone-input/phone-input.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-phone-input',
+  standalone: true,
+  imports: [],
+  templateUrl: './phone-input.component.html',
+  styleUrl: './phone-input.component.scss'
+})
+export class PhoneInputComponent {
+
+}
