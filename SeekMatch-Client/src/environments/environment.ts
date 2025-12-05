931d33d3f0dfb77c56d6d7fab06d3619408e93fe
madnaks <med.haouam@gmail.com>
2025-12-05 15:26:28 -0500

    feat(environment): add environment configuration files for development, staging, and production

---
+++ SeekMatch-Client/src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+    production: false,
+    address: {
+        countries: [
+            { name: 'Canada', code: 'CA' },
+            { name: 'United States', code: 'US' },
+            { name: 'France', code: 'FR' },
+            { name: 'Tunisia', code: 'TN' }
+        ],
+        defaultCountry: 'TN',
+        disableCountryChange: true
+    }
+};
