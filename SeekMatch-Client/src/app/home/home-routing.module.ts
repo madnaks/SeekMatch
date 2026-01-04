1e6a7b95428c062876ebee65483558a59e0311d4
madnaks <med.haouam@gmail.com>
2026-01-03 22:27:21 -0500

    feat(ui): update routing to use JobOffersComponent and enhance job application modals with conditional rendering and stepper

--- SeekMatch-Client/src/app/home/home-routing.module.ts
+++ SeekMatch-Client/src/app/home/home-routing.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './components/home/home.component';
+import { JobOffersComponent } from '../job/components/job-offers/job-offers.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent }
+  { path: '', component: JobOffersComponent }
 ];
 
 @NgModule({
