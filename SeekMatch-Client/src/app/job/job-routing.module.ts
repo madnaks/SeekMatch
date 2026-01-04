fd7c129450c41bef84336ffd3133e73f9a4ebe3b
madnaks <med.haouam@gmail.com>
2026-01-03 21:48:45 -0500

    refactor: remove Companies component and related files to streamline job module

--- SeekMatch-Client/src/app/job/job-routing.module.ts
+++ SeekMatch-Client/src/app/job/job-routing.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { JobOffersComponent } from './components/job-offers/job-offers.component';
-import { CompaniesComponent } from './components/companies/companies.component';
 
 const routes: Routes = [
   { path: 'job-offers', component: JobOffersComponent }, // Default path for the JobModule
-  { path: 'companies', component: CompaniesComponent } // Route with a parameter for job details
 ];
 
 @NgModule({
--- SeekMatch-Client/src/app/job/job.module.ts
+++ SeekMatch-Client/src/app/job/job.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { CompaniesComponent } from './components/companies/companies.component';
 import { JobOffersComponent } from './components/job-offers/job-offers.component';
 import { JobRoutingModule } from './job-routing.module';
 import { TranslateModule } from '@ngx-translate/core';
 import { JobListComponent } from './components/job-list/job-list.component';
 import { JobDetailsComponent } from './components/job-details/job-details.component';
-import { CompanyListComponent } from './components/company-list/company-list.component';
 import { NgbAccordionModule, NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ExpressApplyModalComponent } from './components/express-apply-modal/express-apply-modal.component';
@@ -16,11 +14,9 @@ import { LoaderComponent, LoginButtonComponent } from '@app/shared/ui';
 
 @NgModule({
   declarations: [
-    CompaniesComponent, 
-    JobOffersComponent, 
-    JobListComponent, 
-    JobDetailsComponent, 
-    CompanyListComponent,
+    JobOffersComponent,
+    JobListComponent,
+    JobDetailsComponent,
     ExpressApplyModalComponent
   ],
   imports: [
@@ -40,8 +36,7 @@ import { LoaderComponent, LoginButtonComponent } from '@app/shared/ui';
     ConfirmPasswordInputComponent,
     SharedModule,
     LoginButtonComponent
-],
-  exports: [CompaniesComponent, JobOffersComponent]
+  ]
 })
 
 export class JobModule { }
