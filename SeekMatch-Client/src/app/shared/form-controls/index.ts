408d25c428ff00a2f594c53ed0fae356588289a2
madnaks <med.haouam@gmail.com>
2026-05-22 18:38:49 -0400

    feat: remove full name input component from form controls export

--- SeekMatch-Client/src/app/shared/form-controls/index.ts
+++ SeekMatch-Client/src/app/shared/form-controls/index.ts
@@ -3,7 +3,6 @@ export * from './data-table/data-table.component';
 export * from './date-picker-input/date-picker-input.component';
 export * from './email-input/email-input.component';
 export * from './error-message/error-message.component';
-export * from './full-name-input/full-name-input.component';
 export * from './info/info.component';
 export * from './password-input/password-input.component';
 export * from './phone-input/phone-input.component';
